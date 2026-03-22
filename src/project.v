77e601d8eb3ec39b6999521524d40db5cc4e7254
XxWiwaxX <107466166+XxWiwaxX@users.noreply.github.com>
2026-03-22 23:16:16 +0100

    Rename module tt_um_example to ws_reg_alu

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module ws_reg_alu (
     input  wire [7:0] ui_in,    // Data Input A
     output wire [7:0] uo_out,   // Registered ALU Result
     input  wire [7:0] uio_in,   // Data Input B
